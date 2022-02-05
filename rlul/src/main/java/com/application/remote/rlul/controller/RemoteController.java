4bcce89166c457251935435f1edf1d25c8261ca5
Gunavantha <gunavanth0704@gmail.com>
2022-02-05 12:09:05 +0530

    creating initial directories

--- rlul/src/main/java/com/application/remote/rlul/controller/RemoteController.java
+++ rlul/src/main/java/com/application/remote/rlul/controller/RemoteController.java
@@ -1,2 +1,15 @@
-package com.application.remote.rlul.controller;public class RemoteController {
+package com.application.remote.rlul.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/command")
+public class RemoteController {
+
+    @RequestMapping(value = "/{VIN}/{status}", method = RequestMethod.PUT)
+
+
 }
