88365ca6fc420d93f9adc3026b775fb3137a3e9b
ADITYA S <65755134+Aditya-web-maker@users.noreply.github.com>
2022-02-05 11:10:13 +0530

    Add files via upload

---
+++ rlul/src/main/java/com/application/remote/rlul/RlulApplication.java
@@ -0,0 +1,13 @@
+package com.application.remote.rlul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RlulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RlulApplication.class, args);
+	}
+
+}
---
+++ rlul/src/test/java/com/application/remote/rlul/RlulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.remote.rlul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RlulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
